a80d7671f478af8ef7c5c4757100fd5e1f095426
Fathima2000Irfana <irfanafathima2000786@gmail.com>
2025-09-05 13:03:25 +0530

    twicked the shell script to run without user input

--- setup.sh
+++ setup.sh
@@ -11,7 +11,10 @@ fi
 
 # Step 2: Copy backup files to container
 echo "📂 Copying backup files..."
-docker cp ./backup/* "$CONTAINER_ID":/home/frappe/frappe-bench/sites/frontend/private/backups/
+docker cp ./backups/20250905_121648-frontend-database.sql.gz "$CONTAINER_ID":/home/frappe/frappe-bench/sites/frontend/private/backups/
+docker cp ./backups/20250905_121648-frontend-files.tar "$CONTAINER_ID":/home/frappe/frappe-bench/sites/frontend/private/backups/
+docker cp ./backups/20250905_121648-frontend-private-files.tar "$CONTAINER_ID":/home/frappe/frappe-bench/sites/frontend/private/backups/
+docker cp ./backups/20250905_121648-frontend-site_config_backup.json "$CONTAINER_ID":/home/frappe/frappe-bench/sites/frontend/private/backups/
 
 # Step 3: Restore database and files
 echo "🔄 Restoring backup inside container..."
@@ -19,7 +22,8 @@ docker exec -it "$CONTAINER_ID" bash -c "
 cd /home/frappe/frappe-bench &&
 bench --site frontend --force restore ./sites/frontend/private/backups/20250905_121648-frontend-database.sql.gz \
   --with-public-files ./sites/frontend/private/backups/20250905_121648-frontend-files.tar \
-  --with-private-files ./sites/frontend/private/backups/20250905_121648-frontend-private-files.tar
+  --with-private-files ./sites/frontend/private/backups/20250905_121648-frontend-private-files.tar \
+  --db-root-password admin
 "
 
 # Step 4: Run migrate
